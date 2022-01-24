a6c7154a257ed4a4ff1cd57224ebb56c99550c89
Thomas Grandolph <ThomasGrandolph@aol.com>
2022-01-23 22:05:43 -0500

    Added a navbar above the first navbar

---
+++ src/app/TopNavBar/TopNavBar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'ebay-TopNavBar', 
+    templateUrl: 'TopNavBar.component.html', 
+    styleUrls: ['TopNavBar.component.css']
+})
+export class EbayTopNavBar {
+    title = 'EbayTopNavBar';
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import {EbayNavbar} from 'src/app/navigation/navbar.component';
 import { AppComponent } from './app.component';
 import {EbayFooter} from 'src/app/Footer/Footer.component';
-
+import {EbayTopNavBar} from 'src/app/TopNavBar/TopNavBar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EbayNavbar,
-    EbayFooter
+    EbayFooter,
+    EbayTopNavBar
   ],
   imports: [
     BrowserModule

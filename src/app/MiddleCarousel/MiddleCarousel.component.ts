6dc1dad2eaec46261bb1bf720894d67a11b0107c
Thomas Grandolph <ThomasGrandolph@aol.com>
2022-02-01 22:38:30 -0500

    updating carousel

--- src/app/MiddleCarousel/MiddleCarousel.component.ts
+++ src/app/MiddleCarousel/MiddleCarousel.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
-@Component ({
-    selector: 'ebay-MiddleCarousel', 
-    templateUrl: 'MiddleCarousel.component.html', 
+@Component({
+    selector: 'ebay-MiddleCarousel',
+    templateUrl: 'MiddleCarousel.component.html',
     styleUrls: ['MiddleCarousel.component.css']
 })
 export class EbayMiddleCarousel {
     title = 'EbayMiddleCarousel';
-  }
+    
+    
+    @Input() img: string;
+    @Input() price: number;
+    @Input() discount: string;
+
+    
+
+ constructor() {
+    this.img = "nonsense";
+    this.price = 0;
+    this.discount = "nonsense2";
+
+}
+}
---
+++ src/app/mock_list/mock_list.component.ts
@@ -0,0 +1,22 @@
+export const mock_list = [
+
+{
+    img: "/assets/airpods.jpeg",
+    price:  139.99,
+    discount: ""
+}, 
+
+{
+    img: "/assets/airpods2.jpeg",
+    price:  204.99,
+    discount: ""
+}, 
+{
+    img: "/assets/phonecase.jpeg",
+    price:  15.99,
+    discount: "52% off"
+}, 
+
+];
+
+   

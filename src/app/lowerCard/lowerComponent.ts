50265bfa56eba2e7628772d1b4dccfd4e9ecfd3a
Thomas Grandolph <ThomasGrandolph@aol.com>
2022-02-13 11:39:35 -0500

    no

---
+++ src/app/lowerCard/lowerComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'ebay-lowerCard', 
+    templateUrl: 'lowerCard.component.html', 
+    styleUrls: ['lowerCard.component.css']
+})
+export class EbaylowerCard {
+    title = 'ebaylowerCard';
+  }
